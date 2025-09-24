a8acf076bd3427838cc7d8ea644e1aad7accaedd
Smangamch <smangamch@gmail.com>
2025-09-24 14:08:52 +0200

    Update to the Api files
    Changes to the app settings and the program file

--- src/ClinicQueue.Api/Program.cs
+++ src/ClinicQueue.Api/Program.cs
@@ -1,44 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using EntityFrameworkCore;
+using ClinicQueue.Infrastructure;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplications.CreateBuilder();
+
+
+// Configure the database context to use SQLite by registering the ApplicationDbContext with the dependency injection container
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSQlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? "Data Source=clinicqueue.db"
+    )
+);
+
+//Add services to the container
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// Configure the HTTP request pipeline for the application
+app.UseAuthorization();
+app.MapControllers();
